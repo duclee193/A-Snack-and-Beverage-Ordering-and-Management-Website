e7a204cd3d408a785efd3b4165d73d1f721a0933
Lê Mã Anh Đức <95537428+duclee193@users.noreply.github.com>
2024-12-11 13:37:58 +0700

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,13 +1,5 @@
 <?php
-define('HOST', 'mysql.railway.internal'); // Thay 'localhost' bằng HOST từ thông tin của bạn
-define('USERNAME', 'root'); // Tên người dùng
-define('PASSWORD', 'fmezLMDsweIpcYdCFRzVMtgNQKBlSeKE'); // Mật khẩu
-define('DATABASE', 'railway'); // Tên cơ sở dữ liệu
-
-$connection = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
-
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-?>
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'test1');
